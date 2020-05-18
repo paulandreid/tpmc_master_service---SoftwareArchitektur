e78054840d21deeed37a764d6f77d3f686ede5b7
paulandreid <49744892+paulandreid@users.noreply.github.com>
2020-05-18 14:47:34 +0300

    Update settings.py

--- mtpproject/settings.py
+++ mtpproject/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'mtpproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("POSTGRES_DB"),
-        'USER': env("POSTGRES_USER"),
-        'PASSWORD': env("POSTGRES_PASSWORD"),
-        'HOST': env("POSTGRES_HOST"),
-        'PORT': env("POSTGRES_PORT"),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'DB_NAME',
+        'USER': 'DB_USER',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
