7e91089369d6db9db512f9f1c1bd9689b8ff10b8
paulandreid <49744892+paulandreid@users.noreply.github.com>
2020-05-18 14:42:07 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,16 +5,16 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mtpproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mtpproject.settings') #imports our settings for the project
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as exc:  #check to see if Django is properlly installed
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH constants variable? Did you "
             "forget to activate a virtual constants?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)  #executes the project
 
 
 if __name__ == '__main__':
